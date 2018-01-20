dd577448457ae12da5e49bea8f8db21840b70acf
Sayef Reyadh <sayef.reyadh@github.com>
2018-01-20 20:08:28 +0600

    Doctor Completed

--- MedicAid MVC/MedicAid MVC/Controllers/DoctorController.cs
+++ MedicAid MVC/MedicAid MVC/Controllers/DoctorController.cs
@@ -11,23 +11,37 @@ namespace MedicAid_MVC.Controllers
 {
     public class DoctorController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public DoctorController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Doctor
         public ActionResult Search()
         {
-            var doctors = new List<DoctorModel>
+            /*var doctors = new List<DoctorModel>
             {
                 new DoctorModel(){ Name = "Doctor 1" , HospitalName = "LabAid" , Specialist = "Eye Doctor" , Contact = "01866452" , IsAvailable = true},
                 new DoctorModel(){ Name = "Doctor 2" , HospitalName = "LabAid" , Specialist = "Eye Doctor" , Contact = "01866452" , IsAvailable = true},
                 new DoctorModel(){ Name = "Doctor 3" , HospitalName = "LabAid" , Specialist = "Eye Doctor" , Contact = "01866452" , IsAvailable = false}
 
-            };
+            };*/
 
-            var doctorViewModel = new DoctorViewModel
+            var doctors = _context.Doctors.ToList();
+
+            /*var doctorViewModel = new DoctorViewModel
             {
                 Doctors = doctors
-            };
+            };*/
 
-            return View(doctorViewModel);
+            return View(doctors);
         }
     }
 }
