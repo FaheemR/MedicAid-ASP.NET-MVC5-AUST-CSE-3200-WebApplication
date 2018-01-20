269bad5cc722932e40f6ffc860fdd76ad13de2d0
Sayef Reyadh <sayef.reyadh@github.com>
2018-01-20 19:52:58 +0600

    Database Created for Doctor

--- MedicAid MVC/MedicAid MVC/Models/Authentication/IdentityModels.cs
+++ MedicAid MVC/MedicAid MVC/Models/Authentication/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace MedicAid_MVC.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<DoctorModel> Doctors { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
