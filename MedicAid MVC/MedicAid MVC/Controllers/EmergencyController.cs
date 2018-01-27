6a1206cb5f187e307f9f1ab9d7dee650111349e6
Sayef Reyadh <sayef.reyadh@github.com>
2018-01-27 23:57:04 +0600

    Emergency

--- MedicAid MVC/MedicAid MVC/Controllers/EmergencyController.cs
+++ MedicAid MVC/MedicAid MVC/Controllers/EmergencyController.cs
@@ -20,12 +20,50 @@ namespace MedicAid_MVC.Controllers
         {
             _context.Dispose();
         }
+
         // GET: Emergency
-        public ActionResult Search()
+        public ActionResult Search(bool getOnlyTypes = true, string typeFilter = "", string nameFilter = "", string locationFilter = "")
         {
 
-            var emergency = _context.Emergency.ToList();
-            return View(emergency);
+            if (getOnlyTypes)
+            {
+                var emergencyType = from emergency in _context.Emergency group emergency by emergency.Type into dateGroup select dateGroup.Key;
+
+                List<string> types = new List<string>();
+
+                foreach (var e in emergencyType.ToList())
+                {
+                    types.Add(e);
+                }
+
+                ViewBag.emergencyTypes = types;
+                ViewBag.emergencyTypeCount = types.Count();
+            }
+
+
+            var emergencies = from emergency in _context.Emergency select emergency;
+
+            if (!string.IsNullOrEmpty(typeFilter))
+            {
+                emergencies = emergencies.Where(e => e.Type.Contains(typeFilter));
+            }
+
+            if (!string.IsNullOrEmpty(nameFilter))
+            {
+                emergencies = emergencies.Where(e => e.Name.Contains(nameFilter));
+            }
+
+            if (!string.IsNullOrEmpty(locationFilter))
+            {
+                emergencies = emergencies.Where(e => e.Location.Contains(locationFilter));
+            }
+
+            ViewBag.nameFilter = nameFilter;
+            ViewBag.locationFilter = locationFilter;
+            ViewBag.typeFilter = typeFilter;
+
+            return View(emergencies.ToList());
+
         }
 
         public ActionResult Create()
